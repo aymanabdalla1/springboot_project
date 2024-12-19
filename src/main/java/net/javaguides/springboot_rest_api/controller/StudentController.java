0ae45b364ee7cc587cdfd2521bd382c96988926a
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-12-18 22:30:06 -0600

    path and request

--- src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
@@ -2,6 +2,8 @@ package net.javaguides.springboot_rest_api.controller;
 
 import net.javaguides.springboot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -30,5 +32,24 @@ public class StudentController {
         students.add(new Student(3, "Jane", "Doe"));
         students.add(new Student(4, "John", "Smith"));
         return students;
+    }
+
+    // Spring Boot REST API with Path Variable
+    // {id} = URI template variable
+    // http://localhost:8080/students/1
+    @GetMapping("/students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable(@PathVariable("id") int studentId,
+                                       @PathVariable("first-name") String firstName,
+                                       @PathVariable("last-name") String lastName){
+        return new Student(studentId, firstName, lastName);
+    }
+
+    // Spring Boot REST API with Request Param
+    // http://localhost:8080/students/query?id=1&firstName=Ayman&lastName=Abdalla
+    @GetMapping("/students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName){
+        return new Student(id, firstName, lastName);
     }
 }
