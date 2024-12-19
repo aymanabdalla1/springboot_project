c5a2feb0b82a7d9ab09238cad5e90151f36e30c4
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-12-18 22:10:31 -0600

    more student controller

--- src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot_rest_api/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguides.springboot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -16,5 +19,16 @@ public class StudentController {
                 "Abdalla"
         );
         return student;
+    }
+
+    // http://localhost:8080/students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<Student>();
+        students.add(new Student(1, "Ayman", "Abdalla"));
+        students.add(new Student(2, "John", "Mounce"));
+        students.add(new Student(3, "Jane", "Doe"));
+        students.add(new Student(4, "John", "Smith"));
+        return students;
     }
 }
