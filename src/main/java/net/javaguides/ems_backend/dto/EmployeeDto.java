11e43a845c0c80b4dff817c8738380cfe7ae7951
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2025-01-19 17:30:50 -0600

    adding department id

--- src/main/java/net/javaguides/ems_backend/dto/EmployeeDto.java
+++ src/main/java/net/javaguides/ems_backend/dto/EmployeeDto.java
@@ -14,6 +14,6 @@ public class EmployeeDto {
     private String firstName;
     private String lastName;
     private String email;
-
+    private Long departmentId;
 
 }
--- src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -2,9 +2,11 @@ package net.javaguides.ems_backend.service.impl;
 
 import lombok.AllArgsConstructor;
 import net.javaguides.ems_backend.dto.EmployeeDto;
+import net.javaguides.ems_backend.entity.Department;
 import net.javaguides.ems_backend.entity.Employee;
 import net.javaguides.ems_backend.exception.ResourceNotFoundException;
 import net.javaguides.ems_backend.mapper.EmployeeMapper;
+import net.javaguides.ems_backend.repository.DepartmentRepository;
 import net.javaguides.ems_backend.repository.EmployeeRespository;
 import net.javaguides.ems_backend.service.EmployeeService;
 import org.springframework.stereotype.Service;
@@ -18,10 +20,18 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRespository employeeRespository;
 
+    private DepartmentRepository departmentRepository;
+
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Department department = departmentRepository.findById(employeeDto.getDepartmentId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department not found: " + employeeDto.getDepartmentId()));
+
+        employee.setDepartment(department);
         Employee savedEmployee = employeeRespository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
@@ -49,6 +59,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setFirstName(updatedEmployee.getFirstName());
         employee.setLastName(updatedEmployee.getLastName());
         employee.setEmail(updatedEmployee.getEmail());
+
+        Department department = departmentRepository.findById(updatedEmployee.getDepartmentId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Department not found: " + updatedEmployee.getDepartmentId()));
+
+        employee.setDepartment(department);
 
         Employee updatedEmployeeObj = employeeRespository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
